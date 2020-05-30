3a9a2f676ed9a61a97af82bf4a8c343d18a48826
JadnoABS <vlogggamers@gmail.com>
2020-05-29 21:47:16 -0300

    Front-end da apicação

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg'
+import heroesImg from '../../assets/heroes.png'
+
+function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function hadleLogin(e) {
+        e.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no login. Tente novamente!');
+        }
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt='Be The Hero' />
+
+                <form onSubmit={hadleLogin}>
+                    <h1>Faça seu logon</h1>
+
+                    <input 
+                    placeholder='Sua ID' 
+                    value={id} 
+                    onChange={e => setId(e.target.value)}
+                    />
+                    <button type="submit" className='button'>Entrar</button>
+
+                    <Link className='back-link' to='/register'>
+                        <FiLogIn size={16} color='#E02041' />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={heroesImg} alt='Heroes' />
+        </div>
+    )
+}
+
+
+export default Logon;
